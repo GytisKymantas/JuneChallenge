b3576d7ce3b09e2421624bc3bc1d0e1319c40477
Gytis <Gytis.kymantas@kilo.health>
2024-06-07 01:48:32 +0300

    Initial app integration

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <HeaderNew />
+  <MainNew />
+  <FooterNew />
+</template>
+
+<script>
+import HeaderNew from "./components/HeaderNew.vue";
+import FooterNew from "./components/FooterNew.vue";
+import MainNew from "./components/MainNew.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderNew,
+    FooterNew,
+    MainNew,
+  },
+};
+</script>
+
+<style>
+html {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+</style>
